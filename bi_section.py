eab466d78bb78ae107cefbca6089faadd9f94aac
WiN10 <nawapat.b5911837@gmail.com>
2021-08-30 12:19:10 +0700

    My second Bi-Section.

--- bi_section.py
+++ bi_section.py
@@ -1,18 +1,26 @@
 def bi_section(func, a, b, tol, n_max):
     n = 1
+    c = (a + b) / 2
+    diff = abs(b - a)
+    relative_c_change = 2*tol
+    temp_c = a
 
     def f(x):
         f = eval(func)
         return f
 
-    diff = abs(b - a)
+    while abs(f(c)) > tol and n_max > n:
+        if n >= 2:
+            if relative_c_change <= tol:
+                break
 
-    while diff > tol and n_max >= n:
+        relative_c_change = abs(temp_c-c)/abs(c)
+        temp_c = a
         c = (a + b) / 2
 
         if f(a) * f(b) >= 0:
             print("No root for this equation.")
-            quit()
+            break
         elif f(c) * f(a) < 0:
             b = c
             diff = abs(b - a)
@@ -22,6 +30,7 @@ def bi_section(func, a, b, tol, n_max):
         else:
             print("Something wrong")
 
+        print(f"The relative change is {relative_c_change}")
         print(f"The error is {diff}, the iteration round#{n}")
         print(f"The lower boundary is {a} and the upper boundary is {b}")
         n = n + 1
